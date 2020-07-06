fe7b53ce8461c77a8c979bec3ed7c1360672aa90
Lukec <61413934+LukaPovalej@users.noreply.github.com>
2020-07-06 15:03:15 +0200

    Ball Physics Added

--- Assets/scripts/RightGoalController.cs
+++ Assets/scripts/RightGoalController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player1Controller : MonoBehaviour
+public class RightGoalController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
